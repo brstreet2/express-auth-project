37f8ed017abaabb93126db3935ab366c71af7613
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-03-28 14:40:51 +0700

    feat(user routes): update to new path & add new update and soft delete routes

--- src/routes/auth/user/UserRoutes.ts
+++ src/routes/auth/user/UserRoutes.ts
@@ -1,10 +1,27 @@
 import express from "express";
-import { validateRegisterBody } from "../../../middleware/validation/auth/AuthValidationMiddleware";
 import { authenticate, requireRole } from "../../../middleware/AuthMiddleware";
-import { create, get } from "../../../controllers/auth/user/UserController";
+import {
+  create,
+  get,
+  getAll,
+  softDelete,
+  update,
+} from "../../../controllers/auth/user/UserController";
 import { Roles } from "@prisma/client";
+import {
+  validateCreateBody,
+  validateDeleteParams,
+  validateUpdateRequest,
+} from "../../../requests/UserRequestValidation";
 
 const router = express.Router();
+
+router.get(
+  "/get",
+  authenticate,
+  requireRole(Roles.ADMIN || Roles.SUPER_ADMIN),
+  getAll
+);
 
 router.get(
   "/get/:id",
@@ -15,10 +32,26 @@ router.get(
 
 router.post(
   "/create",
-  validateRegisterBody,
+  validateCreateBody,
   authenticate,
   requireRole(Roles.ADMIN || Roles.SUPER_ADMIN),
   create
+);
+
+router.put(
+  "/update/:id",
+  validateUpdateRequest,
+  authenticate,
+  requireRole(Roles.ADMIN || Roles.SUPER_ADMIN),
+  update
+);
+
+router.delete(
+  "/delete/:id",
+  validateDeleteParams,
+  authenticate,
+  requireRole(Roles.ADMIN || Roles.SUPER_ADMIN),
+  softDelete
 );
 
 export default router;
