e0c1a2792abf36ec4206da5841865be1fe6a3a35
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-03-28 13:46:15 +0700

    feat(user controller): get method

--- src/controllers/auth/user/UserController.ts
+++ src/controllers/auth/user/UserController.ts
@@ -1,9 +1,19 @@
-import { Request, Response } from "express";
-import { createUser, getUserByEmail } from "../../../services/auth/UserService";
+import { Response } from "express";
+import {
+  createUser,
+  getUserByEmail,
+  getUserById,
+} from "../../../services/auth/UserService";
+import { AuthenticatedRequest } from "../../../interface/AuthenticatedRequest";
 
-export async function create(req: Request & { user?: any }, res: Response) {
+export async function create(req: AuthenticatedRequest, res: Response) {
   const { email, password, role } = req.body;
   const user = req.user;
+
+  if (!user) {
+    return res.status(401).json({ message: "User is not authenticated." });
+  }
+
   try {
     const existingUser = await getUserByEmail(email);
     if (existingUser) {
@@ -11,6 +21,28 @@ export async function create(req: Request & { user?: any }, res: Response) {
     }
     await createUser(email, password, role, user.id);
     res.status(201).json({ message: "User created successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+}
+
+export async function get(req: AuthenticatedRequest, res: Response) {
+  const user = req.user;
+  const userId = req.params.id;
+
+  if (!user) {
+    return res.status(401).json({ message: "User is not authenticated." });
+  }
+
+  try {
+    const userDb = await getUserById(userId);
+
+    if (!userDb) {
+      res.status(404).json({ message: "User not found." });
+    }
+
+    res.status(200).json(userDb);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error." });
